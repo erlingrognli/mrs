9b302a1fb614d3de4555cad4a2776d15e099a3d7
Erling Wright Rognli <erlrog@ahus.no>
2024-06-04 18:40:22 +0200

    Initial commit, updated Readme and code for analysis of synthetic data.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# mrs
+# Brain Imaging in Youth with OCD and Psychosis
+
+Code for data processing and analysis in a study of brain imaging in youth with OCD and psychosis.
+
+Code assumes that data is stored in a folder called mrs_data, at the same location as the folder containing the R-project file.
---
+++ analyse_synt_data.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+
+d <- read.csv('~/mrs_data/Synthetic_neuroimaging_data.csv') %>%
+  
+    select(SubjID, Age, Sex, Diagnosis, Volume_1:Volume_5) %>%
+    
+    mutate(id = 1:length(SubjID), 
+           age = Age, 
+           female = ifelse(Sex == 'male', 0, 1), 
+           schiz = ifelse(Diagnosis == 'CTR', 0, 1), 
+           .keep = 'unused') %>%
+  
+    pivot_longer(cols = Volume_1:Volume_5, 
+                 names_to = 'volume_id', 
+                 names_prefix = "Volume_", 
+                 values_to = 'volume', 
+                 names_transform = list(volume_id = as.integer))
+
+library(brms)
+
+mod <- brmsformula(volume ~ s(age) + (1|id) + (1|volume_id) + female + schiz,
+                   sigma ~ (1|volume), # letting errors vary by volume
+                   family = 'gaussian')
+
+get_prior(mod, data = d)
+
+pr <- prior('normal(0, 5)', 'b')
+
+fit <- brm(mod, data = d, prior = pr)
+
