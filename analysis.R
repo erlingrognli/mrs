5df876d81a51ecb148e7312618afafb3304ff667
Erling Wright Rognli <erlrog@ahus.no>
2024-09-09 18:44:05 +0200

    Functions to synthesise data + a synthesised dataset.

--- analysis.R
+++ analysis.R
@@ -10,19 +10,16 @@ vPFC_thickness <- c("rh_lateralorbitofrontal_thickness",
 vPFC_area <- c("rh_lateralorbitofrontal_area", 
                "lh_lateralorbitofrontal_area", 
                "rh_medialorbitofrontal_area", 
-               "lh_medialorbitofrontal_area")
+               "lh_medialorbitofrontal_area",
+               'lh_vPFC_area',
+               'rh_vPFC_area')
 
 vPFC <- c(vPFC_area, vPFC_thickness)
-
-occipital_thickness <- c("lh_lateraloccipital_thickness",
-                         "rh_lateraloccipital_thickness")
 
 occipital_area <- c("lh_lateraloccipital_area", 
                     "rh_lateraloccipital_area")
 
-occipital <- c(occipital_area, occipital_thickness)
-
-all_areas <- c(caudate, vPFC, occipital)
+all_areas <- c(caudate, vPFC, occipital_area)
 
 d <- read_csv(file = '~/mrs_data/mrs_wf_data.csv',
               show_col_types = F) %>%
@@ -30,6 +27,8 @@ d <- read_csv(file = '~/mrs_data/mrs_wf_data.csv',
   filter(is.na(age) == F) %>%
   
   mutate(id = 1:length(SubjID),
+         lh_vPFC_area = lh_lateralorbitofrontal_area + lh_medialorbitofrontal_area,
+         rh_vPFC_area = rh_lateralorbitofrontal_area + rh_medialorbitofrontal_area,
          .keep = 'unused') %>%
   
   pivot_longer(cols = !c(id, gender, age, ocd, scz), 
@@ -38,43 +37,55 @@ d <- read_csv(file = '~/mrs_data/mrs_wf_data.csv',
   
   filter(area_name %in% all_areas)
 
-d <- mutate(d, 
-            ind_beta = 
-              case_when(ocd == 1 & area_name %in% vPFC ~ 1,
-                        ocd == 1 & area_name %in% caudate ~ 2,
-                        ocd == 1 & area_name %in% occipital ~ 3,
-                        ocd == 0 ~ 3),
-            ind_area = 
-              case_when(area_name %in% vPFC_area ~ 1,
-                        area_name %in% vPFC_thickness ~ 2,
-                        area_name %in% caudate ~ 3,
-                        area_name %in% occipital_area ~ 4,
-                        area_name %in% occipital_thickness ~ 5),
+d <- mutate(d, ind_area = 
+              case_when(area_name %in% vPFC_thickness ~ 1,
+                        area_name %in% caudate ~ 2,
+                        area_name %in% vPFC_area ~ 3,
+                        area_name %in% occipital_area ~ 4),
             .keep = 'all')
+
+ind_pred <- mutate(d, 
+                   gender = ifelse(gender == 1, -.5, .5),
+                   vpfc_area_ocd = ifelse(area_name %in% vPFC_area & ocd == 1, 1, 0),
+                   vpfc_thickness_ocd = ifelse(area_name %in% vPFC_thickness & ocd == 1, 1, 0),
+                   caudate_ocd = ifelse(area_name %in% caudate & ocd == 1, 1, 0),
+                   occipital_area_ocd = ifelse(area_name %in% occipital_area & ocd == 1, 1, 0),
+                   .keep = 'none')
 
 ggplot(data = d,
        mapping = aes(age, log(mri))) +
   geom_point() + 
   geom_smooth()
 
+
+
 dat <- list(N = length(unique(d$id)),
             n_obs = nrow(d),
             n_area = max(d$ind_area),
-            n_beta = max(d$ind_beta),
+            n_beta = ncol(ind_pred),
             ind_id = d$id,
-            ind_beta_ocd = d$ind_beta,
             ind_area = d$ind_area,
-            ind_gender = d$gender,
+            ind_pred = as.matrix(ind_pred),
             mri = d$mri,
             age = d$age,
             n_knots = 5,
             age_knots = c(11.0, 13.0, 15.0, 17.0, 19.0))
+
+
       
 m <- cmdstan_model('mri_mod.stan')
 
 fit <- m$sample(data = dat)
 
-mcmc_pairs(fit$draws(variables = c('sigma', 'beta_ocd', 'beta_gender')))
+png(file = 'pairs.png',
+    width = 45,
+    height = 45,
+    units = 'cm',
+    res = 100)
+
+mcmc_pairs(fit$draws(variables = c('area_icpt', 'beta', 'knot_values', 'alpha')))
+
+dev.off()
 
 ppc_draws <- fit$draws(variables = 'ppc', format = 'draws_matrix')
 
@@ -85,7 +96,7 @@ ppc_violin_grouped(y = log(d$mri),
 
 ppc_pit_ecdf(y = log(d$mri), yrep = ppc_draws)
 
-betas <- summarise_draws(fit$draws(variables = c('beta', 'beta_female', 'sigma')))
+betas <- summarise_draws(fit$draws(variables = c('beta', 'sigma')))
 
 areas <- summarise_draws(fit$draws(variables = c('area_icpt', 'var_area_icpt')))
 
