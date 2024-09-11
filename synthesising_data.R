833c855f1542ba7ab3ca949da8e9103e5163ea83
Erling Wright Rognli <erlrog@ahus.no>
2024-09-11 09:17:50 +0200

    Added age to synthetic data.

--- synthesising_data.R
+++ synthesising_data.R
@@ -1,3 +1,5 @@
+library(tidyverse); library(synthpop)
+
 d <- read_csv(file = '~/mrs_data/mrs_wf_data.csv',
               show_col_types = F) %>%
   
@@ -6,6 +8,14 @@ d <- read_csv(file = '~/mrs_data/mrs_wf_data.csv',
   mutate(id = 1:length(SubjID),
          lh_vPFC_area = lh_lateralorbitofrontal_area + lh_medialorbitofrontal_area,
          rh_vPFC_area = rh_lateralorbitofrontal_area + rh_medialorbitofrontal_area,
-         .keep = 'unused') # %>%
+         .keep = 'unused') %>%
 
-d_s <- select(d, c('ocd', 'gender', caudate, occipital_area, 'lh_vPFC_area', 'rh_vPFC_area'))
+  select(all_of(c('age', 'ocd', 'gender', 'Left.Caudate', 'Right.Caudate', 
+              "lh_lateraloccipital_area", "rh_lateraloccipital_area", 
+              'lh_vPFC_area', 'rh_vPFC_area'))) %>%
+  
+  mutate(ocd = as.factor(ocd), gender = as.factor(gender))
+
+d_syn <- syn(d)
+
+d <- d_syn$syn
